9575eddc51c0546256592731d1a42c4bb2ceb274
rizkialifah <50816275+rizkialifah@users.noreply.github.com>
2023-06-15 07:22:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import os
 #import requests
 
 title = []
